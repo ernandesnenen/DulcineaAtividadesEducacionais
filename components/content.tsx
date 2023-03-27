540d4075baf1d53510acfbab61f0ac72deeb402a
ernandes <nenenernan@gmail.com>
2023-03-27 19:35:49 -0300

    somar

--- components/content.tsx
+++ components/content.tsx
@@ -30,7 +30,7 @@ export  function Content() {
                 <p className="font-bold py-2">Família do B</p>
                 <p className="py-2">Esta atividade tem com objetivo forma palavras usando a família do B, ba, be, bi, bo, bu. </p>
               </Link>
-          </div>
+          </div>        
 
           <div className="
            w-full
@@ -42,6 +42,19 @@ export  function Content() {
               <Link href={"https://scratch.mit.edu/projects/825172665"} target="_blank">
                 <p className="font-bold py-2">J ou G</p>
                 <p className="py-2">Esta atividade tem com objetivo dizer se a palavra é com J ou G. </p>
+              </Link>
+          </div>
+
+          <div className="
+           w-full
+            border-2
+            rounded-lg
+           border-slate-300
+           shadow-lg
+           p-4">
+              <Link href={"https://scratch.mit.edu/projects/818663152"} target="_blank">
+                <p className="font-bold py-2">Somar</p>
+                <p className="py-2">Esta atividade tem com objetivo somar numero aleatórios. </p>
               </Link>
           </div>
 
